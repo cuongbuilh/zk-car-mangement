e5032e38e56a18b646df2ec6d826ecea548a6429
namchtq238 <kaizuto123@gmail.com>
2022-08-12 16:48:56 +0700

    fix layout add form

--- src/main/java/com/example/zktraining/controller/AddCarController.java
+++ src/main/java/com/example/zktraining/controller/AddCarController.java
@@ -1,13 +1,14 @@
 package com.example.zktraining.controller;
 
+import com.example.zktraining.dto.CarDTO;
+import lombok.Getter;
+import lombok.Setter;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zk.ui.select.annotation.WireVariable;
-import org.zkoss.zk.ui.util.Composer;
 import org.zkoss.zkplus.spring.DelegatingVariableResolver;
 import org.zkoss.zul.Window;
 
@@ -15,6 +16,10 @@ import org.zkoss.zul.Window;
 public class AddCarController extends SelectorComposer<Component> {
     @Wire
     Window window;
+
+    @Getter
+            @Setter
+    CarDTO car;
     @Listen("onClick = #closeBtn")
     public void showModal(Event event){
         window.detach();
